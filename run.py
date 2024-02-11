7975c57961a94c55c30325a708823dc88f27af4b
Rachel Lazarus <rochilazarus@gmail.com>
2024-02-11 00:40:02 +0000

    Add validate_expense_category to check that only a number between 1 and 5 was entered

--- run.py
+++ run.py
@@ -14,12 +14,33 @@ SHEET = GSPREAD_CLIENT.open('expense-tracker')
 
 
 def get_expense_category():
+    """
+    Get the user to choose a category for the expense.
+    """
     print(Fore.WHITE + 'Please choose a number for one of the following categories:\n')
     print(Fore.BLUE + '1 - Food & Drink \n2 - Entertainment \n3 - Travel \n4 - Basics and Hygiene \n5 - Extras\n')
     category = input(Fore.WHITE + "Enter category number:" )
     return category
 
+
+def validate_expense_category(data):
+    """
+    Inside the try converts data entered into an integer,
+    Raises ValueError if the data entered can't be converted
+    into an integer, or is not between 1 and 5.
+    """
+    try:
+        int(data) 
+        if int(data) < 1 or int(data) > 5:
+            raise ValueError(
+                f"Expected a number between 1 and 5, you provided {data}"
+            )
+    except ValueError as e:
+        print(f"Invalid category: {e}, please try again.\n")
+
+
 def main():
-    get_expense_category()
+    category = get_expense_category()
+    validate_expense_category(category)
 
 main()
