6399d237348d9afb6b9596d7b3266e6f602624dd
Rachel Lazarus <rochilazarus@gmail.com>
2024-02-11 00:04:46 +0000

    Create get_expense_category function for the user to choose the expense category and create main function to run program.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from colorama import Fore
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,8 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expense-tracker')
 
-feb = SHEET.worksheet('February')
 
-data = feb.get_all_values()
+def get_expense_category():
+    print(Fore.WHITE + 'Please choose a number for one of the following categories:\n')
+    print(Fore.BLUE + '1 - Food & Drink \n2 - Entertainment \n3 - Travel \n4 - Basics and Hygiene \n5 - Extras\n')
+    category = input(Fore.WHITE + "Enter category number:" )
+    return category
 
-print(data)
+def main():
+    get_expense_category()
+
+main()
